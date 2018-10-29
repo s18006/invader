5df3b5cf1adb17ebc64d5726322a090aa9cc133f
Kecsa <s18006@std.it-college.ac.jp>
2018-10-29 03:24:15 +0000

    new version

--- js/invader.js
+++ js/invader.js
@@ -3,6 +3,20 @@ phina.globalize();
 const SCREEN_WIDTH = 960;
 const SCREEN_HEIGHT = 640;
 
+phina.define('Mainscene', {
+    superClass: "DisplayScene',
+    init: function() {
+        this.superInit({
+            width: SCREEN_WIDTH;
+            height: SCREEN_HEIGHT;
+        });
+        this.griX = Grid(SCREEN_WIDTH, 40);
+        this.gridY = Grid(SCREEN_HEIGHT, 40);
+
+        this.backgroundColor = 'black';
+    }
+});
+
 phina.main(() => {
     const app = GameApp({
         title: "インベーター";
